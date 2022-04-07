4c483f4923062051e52b25012740b4ff9203ae65
María Soledad García Tejeda <msgarciatejeda@hotmail.com>
2022-04-07 20:07:34 -0300

    Desafío Clase 6_Incorporar Arrays

--- index.js
+++ index.js
@@ -1,100 +1,183 @@
-  //Lista de precios:
-const abrazaderas = 10;
-const accesoriosAgua = 15;
-const descuento = 0.5;
+//LISTADO DE PRODUCTOS
 
+function mayorQue (i){ return (a) => a>i};
 
-let unidadesAbrazadera2 = 1;
-let precioAbrazaderaMay = 1;
-let precioAbrazaderaMin = 1;
-let consulta = 0;
-let unidadesAgua2 = 1;
-let precioAguaMay = 1;
-let precioAguaMin = 1;
-let consulta2 = 0;
+let mayorQueCero = mayorQue (0);
 
-function totalMinorista(cantidad, precio) {
-    let resultado = cantidad * precio;
-    return resultado;
+ function Producto (id, nombre, precio, cantidadMayorista, stock){
+    this. id=id;
+    this. nombre= nombre;
+    this. precio= precio;
+    this. cantidadMayorista= cantidadMayorista;
+    this. stock= stock;
+    this. enStock= mayorQueCero (stock); 
 }
 
-function totalMayorista(cantidad, precio, descuento) {
-    let resultado = cantidad * precio * descuento;
-    return resultado;
+const producto1= new Producto (1, "ABRAZADERAS", 5, 20,100);
+const producto2= new Producto (2, "ACCESORIOS EN BRONCE FUNDIDO ROSCADO", 10, 50, 200);
+const producto3= new Producto (3, "ACCESORIOS EN BRONCE PARA GAS", 15, 25, 0);
+
+
+const listaProductos = [producto1, producto2, producto3]; 
+
+
+//DESCUENTO MAYORISTA
+
+let descuentoMayorista= 0.5;
+
+const listaProductosMayorista = listaProductos.map((el) => {
+    return {
+        nombre: el.nombre,
+        precio: el.precio * descuentoMayorista
+    }
+})
+
+
+
+//COMPRA
+
+let unidadesProducto1=0;
+let unidadesProducto2=0;
+let unidadesProducto3=0;
+let consulta=0;
+
+
+
+//COMPRA DEL PRODUCTO 1
+
+function fProducto1() {
+    if (listaProductos[0].stock > 0) {
+        unidadesProducto1 = parseInt(prompt("El precio por unidad de " + listaProductos[0].nombre + " es de $" + listaProductos[0].precio + ".\n Para acceder a un descuento mayorista del " + (descuentoMayorista * 100) + "%, debe comprar un mínimo de " + listaProductos[0].cantidadMayorista + " unidades.\n ¿Cuántas unidades quiere comprar?"));
+
+        if (unidadesProducto1 < listaProductos[0].stock) {
+
+            if (unidadesProducto1 >= listaProductos[0].cantidadMayorista) {
+                alert("Aplica descuento mayorista del " + (descuentoMayorista * 100) + "% \n Su total es de $" + (listaProductosMayorista[0].precio * unidadesProducto1));
+            } else {
+                alert("La cantidad ingresada corresponde al precio minorista \n Su total es de $" + (listaProductos[0].precio * unidadesProducto1));
+            }
+
+            consulta = parseInt(prompt("¿Desea seguir comprando? \n Presione 1 para continuar o 2 para terminar"));
+
+            if (consulta == 2 && unidadesProducto1 > listaProductos[0].cantidadMayorista) {
+                alert("¡Gracias por su compra! \n Su total de " + listaProductos[0].nombre + " es de $" + (listaProductosMayorista[0].precio * unidadesProducto1) + ".\n Si compró otros productos presione el botón de TOTAL");
+            } else if (consulta == 2 && unidadesProducto1 < listaProductos[0].cantidadMayorista) {
+                alert("¡Gracias por su compra! \n Su total de " + listaProductos[0].nombre + " es de $" + (listaProductos[0].precio * unidadesProducto1) + ".\n Si compró otros productos presione el botón de TOTAL");
+            } else if (consulta == 1) {
+                alert("Prosiga con su compra");
+            } else {
+                alert("Opción inválida");
+            }
+        } else {alert("Lo sentimos, no tenemos la cantidad solicitada. \n Por favor, ingrese una cantidad menor.");
+    }} else {
+        alert("Lo sentimos, el producto está fuera de Stock")
+    }
 }
 
 
-function unidadesAbrazadera() {
-    unidadesAbrazadera2 = parseInt(prompt("El precio minorista por unidad es de $" + abrazaderas + "\n ¿Cuántas unidades quiere comprar?"));
-    precioAbrazaderaMay = totalMayorista(unidadesAbrazadera2, abrazaderas, descuento);
-    precioAbrazaderaMin = totalMinorista(unidadesAbrazadera2, abrazaderas);
-    if (unidadesAbrazadera2 > 10) {
-        alert("Aplica descuento mayorista del " + (descuento * 100) + "% \n Su total es de $" + precioAbrazaderaMay);
-    } else {
-        alert("La cantidad ingresada corresponde al precio minorista \n Su total es de $" + precioAbrazaderaMin);
+
+//COMPRA DEL PRODUCTO 2
+
+function fProducto2() {
+    if (listaProductos[1].stock > 0) {
+        unidadesProducto2 = parseInt(prompt("El precio por unidad de " + listaProductos[1].nombre + " es de $" + listaProductos[1].precio + ".\n Para acceder a un descuento mayorista del " + (descuentoMayorista * 100) + "%, debe comprar un mínimo de " + listaProductos[1].cantidadMayorista + " unidades.\n ¿Cuántas unidades quiere comprar?"));
+
+        if (unidadesProducto2 < listaProductos[1].stock) {
+
+            if (unidadesProducto2 >= listaProductos[1].cantidadMayorista) {
+                alert("Aplica descuento mayorista del " + (descuentoMayorista * 100) + "% \n Su total es de $" + (listaProductosMayorista[1].precio * unidadesProducto2));
+            } else {
+                alert("La cantidad ingresada corresponde al precio minorista \n Su total es de $" + (listaProductos[1].precio * unidadesProducto2));
+            }
+
+            consulta = parseInt(prompt("¿Desea seguir comprando? \n Presione 1 para continuar o 2 para terminar"));
+
+            if (consulta == 2 && unidadesProducto2 > listaProductos[1].cantidadMayorista) {
+                alert("¡Gracias por su compra! \n Su total de " + listaProductos[1].nombre + " es de $" + (listaProductosMayorista[1].precio * unidadesProducto2) + ".\n Si compró otros productos presione el botón de TOTAL");
+            } else if (consulta == 2 && unidadesProducto2 < listaProductos[1].cantidadMayorista) {
+                alert("¡Gracias por su compra! \n Su total de " + listaProductos[1].nombre + " es de $" + (listaProductos[1].precio * unidadesProducto2) + ".\n Si compró otros productos presione el botón de TOTAL");
+            } else if (consulta == 1) {
+                alert("Prosiga con su compra");
+            } else {
+                alert("Opción inválida");
+            }
+        } else {alert("Lo sentimos, no tenemos la cantidad solicitada. \n Por favor, ingrese una cantidad menor.")
+    }}else {
+        alert("Lo sentimos, el producto está fuera de Stock")
     }
+}
 
-    consulta = parseInt(prompt("¿Desea seguir comprando? \n Presione 1 para continuar o 2 para terminar"));
 
-    if (consulta == 2 && unidadesAbrazadera2 > 10) {
-        alert("¡Gracias por su compra! \n Su total de Abrazaderas es de $" + precioAbrazaderaMay);
-    } else if (consulta == 2 && unidadesAbrazadera2 < 10) {
-        alert("¡Gracias por su compra! \n Su total de Abrazaderas es de $" + precioAbrazaderaMin);
-    } else if (consulta == 1) {
-        alert("Prosiga con su compra");
-    } else {
-        alert("Opción inválida");
+//COMPRA DEL PRODUCTO 3
+
+function fProducto3() {
+    if (listaProductos[2].stock > 0) {
+        unidadesProducto3 = parseInt(prompt("El precio por unidad de " + listaProductos[2].nombre + " es de $" + listaProductos[2].precio + ".\n Para acceder a un descuento mayorista del " + (descuentoMayorista * 100) + "%, debe comprar un mínimo de " + listaProductos[2].cantidadMayorista + " unidades.\n ¿Cuántas unidades quiere comprar?"));
+
+        if (unidadesProducto3 < listaProductos[2].stock) {
+
+            if (unidadesProducto3 >= listaProductos[2].cantidadMayorista) {
+                alert("Aplica descuento mayorista del " + (descuentoMayorista * 100) + "% \n Su total es de $" + (listaProductosMayorista[2].precio * unidadesProducto3));
+            } else {
+                alert("La cantidad ingresada corresponde al precio minorista \n Su total es de $" + (listaProductos[2].precio * unidadesProducto3));
+            }
+
+            consulta = parseInt(prompt("¿Desea seguir comprando? \n Presione 1 para continuar o 2 para terminar"));
+
+            if (consulta == 2 && unidadesProducto3 > listaProductos[2].cantidadMayorista) {
+                alert("¡Gracias por su compra! \n Su total de " + listaProductos[2].nombre + " es de $" + (listaProductosMayorista[2].precio * unidadesProducto3) + ".\n Si compró otros productos presione el botón de TOTAL");
+            } else if (consulta == 2 && unidadesProducto3 < listaProductos[2].cantidadMayorista) {
+                alert("¡Gracias por su compra! \n Su total de " + listaProductos[2].nombre + " es de $" + (listaProductos[2].precio * unidadesProducto3) + ".\n Si compró otros productos presione el botón de TOTAL");
+            } else if (consulta == 1) {
+                alert("Prosiga con su compra");
+            } else {
+                alert("Opción inválida");
+            }
+        } else { alert("Lo sentimos, no tenemos la cantidad solicitada. \n Por favor, ingrese una cantidad menor.")
+    }} else {
+        alert("Lo sentimos, el producto está fuera de Stock")
     }
 }
 
 
 
-function unidadesAgua() {
-    unidadesAgua2 = parseInt(prompt("El precio minorista por unidad es de $" + accesoriosAgua + "\n ¿Cuántas unidades quiere comprar?"));
-    precioAguaMay = totalMayorista(unidadesAgua2, accesoriosAgua, descuento);
-    precioAguaMin = totalMinorista(unidadesAgua2, accesoriosAgua);
-    if (unidadesAgua2 > 10) {
-        alert("Aplica descuento mayorista del " + (descuento * 100) + "% \n Su total es de $" + precioAguaMay);
-    } else {
-        alert("La cantidad ingresada corresponde al precio minorista \n Su total es de $" + precioAguaMin);
-    }
-
-    consulta2 = parseInt(prompt("¿Desea seguir comprando? \n Presione 1 para continuar o 2 para terminar"));
-
-    if (consulta2 == 2 && unidadesAgua2 > 10) {
-        alert("¡Gracias por su compra! \n Su total de Accesorios en Bronce Fundido es de $" + precioAguaMay);
-    } else if (consulta2 == 2 && unidadesAgua2 < 10) {
-        alert("¡Gracias por su compra! \n Su total de Accesorios en Bronce Fundido es de $" + precioAguaMin);
-    } else if (consulta2 == 1) {
-        alert("Prosiga con su compra.");
-    } else {
-        alert("Opción inválida");
-    }
-}
-
-
+//TOTAL DE LA COMPRA
 
 function total() {
-    while (consulta == 1 || consulta2 == 1 || consulta == 2 || consulta2 == 2 ) {
+    while (consulta == 1  || consulta == 2 ) {
         switch (true) {
-
-            case (unidadesAbrazadera2 > 10 && unidadesAgua2 > 10):
-                let suma1 = precioAbrazaderaMay + precioAguaMay;
+            case (unidadesProducto1> listaProductos[0].cantidadMayorista && unidadesProducto2 > listaProductos[1].cantidadMayorista && unidadesProducto3 > listaProductos[2].cantidadMayorista):
+                let suma1 = (listaProductosMayorista[0].precio*unidadesProducto1) + (listaProductosMayorista[1].precio*unidadesProducto2)+ (listaProductosMayorista[2].precio*unidadesProducto3);
                 alert("¡Gracias por su compra! \n Su total es de $" + suma1);
                 break;
-            case (unidadesAbrazadera2 > 10 && unidadesAgua2 < 10):
-                let suma2 = precioAbrazaderaMay + precioAguaMin;
+            case (unidadesProducto1> listaProductos[0].cantidadMayorista && unidadesProducto2 > listaProductos[1].cantidadMayorista && unidadesProducto3 < listaProductos[2].cantidadMayorista):
+                let suma2 = (listaProductosMayorista[0].precio*unidadesProducto1) + (listaProductosMayorista[1].precio*unidadesProducto2)+ (listaProductos[2].precio*unidadesProducto3);
                 alert("¡Gracias por su compra! \n Su total es de $" + suma2);
                 break;
-            case (unidadesAbrazadera2 < 10 && unidadesAgua2 < 10):
-                let suma3 = precioAbrazaderaMin + precioAguaMin;
+            case (unidadesProducto1> listaProductos[0].cantidadMayorista && unidadesProducto2 < listaProductos[1].cantidadMayorista && unidadesProducto3 < listaProductos[2].cantidadMayorista):
+                let suma3 = (listaProductosMayorista[0].precio*unidadesProducto1) + (listaProductos[1].precio*unidadesProducto2)+ (listaProductos[2].precio*unidadesProducto3);
                 alert("¡Gracias por su compra! \n Su total es de $" + suma3);
                 break;
-            case (unidadesAbrazadera2 < 10 && unidadesAgua2 > 10):
-                let suma4 = precioAbrazaderaMin + precioAguaMay;
+            case (unidadesProducto1< listaProductos[0].cantidadMayorista && unidadesProducto2 < listaProductos[1].cantidadMayorista && unidadesProducto3 < listaProductos[2].cantidadMayorista):
+                let suma4 = (listaProductos[0].precio*unidadesProducto1) + (listaProductos[1].precio*unidadesProducto2)+ (listaProductos[2].precio*unidadesProducto3);
                 alert("¡Gracias por su compra! \n Su total es de $" + suma4);
                 break;
-                
+            case (unidadesProducto1< listaProductos[0].cantidadMayorista && unidadesProducto2 < listaProductos[1].cantidadMayorista && unidadesProducto3 > listaProductos[2].cantidadMayorista):
+                let suma5 = (listaProductos[0].precio*unidadesProducto1) + (listaProductos[1].precio*unidadesProducto2)+ (listaProductosMayorista[2].precio*unidadesProducto3);
+                alert("¡Gracias por su compra! \n Su total es de $" + suma5);
+                break;
+            case (unidadesProducto1< listaProductos[0].cantidadMayorista && unidadesProducto2 > listaProductos[1].cantidadMayorista && unidadesProducto3 > listaProductos[2].cantidadMayorista):
+                let suma6 = (listaProductos[0].precio*unidadesProducto1) + (listaProductosMayorista[1].precio*unidadesProducto2)+ (listaProductosMayorista[2].precio*unidadesProducto3);
+                alert("¡Gracias por su compra! \n Su total es de $" + suma6);
+                break;
+            case (unidadesProducto1> listaProductos[0].cantidadMayorista && unidadesProducto2 < listaProductos[1].cantidadMayorista && unidadesProducto3 > listaProductos[2].cantidadMayorista):
+                let suma7 = (listaProductosMayorista[0].precio*unidadesProducto1) + (listaProductos[1].precio*unidadesProducto2)+ (listaProductosMayorista[2].precio*unidadesProducto3);
+                alert("¡Gracias por su compra! \n Su total es de $" + suma7);
+                break;
+            case (unidadesProducto1< listaProductos[0].cantidadMayorista && unidadesProducto2 > listaProductos[1].cantidadMayorista && unidadesProducto3 < listaProductos[2].cantidadMayorista):
+                let suma8 = (listaProductos[0].precio*unidadesProducto1) + (listaProductosMayorista[1].precio*unidadesProducto2)+ (listaProductos[2].precio*unidadesProducto3);
+                alert("¡Gracias por su compra! \n Su total es de $" + suma8);
+                break;           
             default:
                 break;
         }
@@ -102,3 +185,14 @@ function total() {
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+ 
